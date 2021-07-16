5329796d5a0d2bcaf9697c90ed17b9090b8c5ffc
eliana <elianarlivingston@gmail.com>
2021-07-15 21:45:30 -0300

    add EmptyList component

---
+++ src/components/EmptyList.vue
@@ -0,0 +1,46 @@
+<template>
+  <article class="empty-card">
+    <h1 class="empty-card__title">Uh-oh!</h1>
+
+    <p class="empty-card__text">You look lost on your journey!</p>
+
+    <router-link to="/discover">
+      <Button @click="goToHome">Go back home</Button>
+    </router-link>
+  </article>
+</template>
+
+<script>
+import { Button } from "./index";
+import { useRouter } from "vue-router";
+
+export default {
+  name: "EmptyList",
+  components: { Button },
+  setup() {
+    const router = useRouter();
+
+    const goToHome = () => router.push("/discover");
+
+    return { goToHome };
+  },
+};
+</script>
+
+<style scoped>
+.empty-card {
+  text-align: center;
+}
+.empty-card__title {
+  font-size: 36px;
+  font-weight: bold;
+  line-height: 43.2px;
+  color: var(--gray-600);
+}
+.empty-card__text {
+  font-size: 20px;
+  line-height: 30px;
+  color: var(--gray-500);
+  text-align: center;
+}
+</style>
