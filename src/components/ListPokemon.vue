62b6bb93926ea56dd17aaff0e50f7391735f5807
eliana <elianarlivingston@gmail.com>
2021-07-15 21:46:17 -0300

    add ListPokemon component

---
+++ src/components/ListPokemon.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="list">
+      <span>{{ name }}</span>
+      <ButtonFavorite :favorite="favorite" />
+  </li>
+</template>
+
+<script>
+import { ButtonFavorite } from './index'
+
+export default {
+    name: 'ListPokemon',
+    props: {
+        name: String,
+        favorite: Boolean
+    },
+    components: { ButtonFavorite }
+}
+</script>
+
+<style scoped>
+.list {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    font-size: 22px;
+    color: var(--gray-600);
+    padding: .4em .4em .4em 1em;
+    border-radius: var(--rounded);
+    background-color: var(--gray-50);
+}
+</style>
